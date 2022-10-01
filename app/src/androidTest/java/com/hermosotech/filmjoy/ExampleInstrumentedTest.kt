ff5c4c2bd27d24c440e96eeaf715bf8507ee330c
manuelalejandro <manuel.hermoso@nicepeopleatwork.com>
2022-10-01 16:54:46 +0200

    rename package

--- app/src/androidTest/java/com/hermosotech/filmjoy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hermosotech/filmjoy/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.hermosotech.myapplication
+package com.hermosotech.filmjoy
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hermosotech.myapplication", appContext.packageName)
+        assertEquals("com.hermosotech.filmjoy", appContext.packageName)
     }
 }
--- app/src/main/java/com/hermosotech/filmjoy/MainActivity.kt
+++ app/src/main/java/com/hermosotech/filmjoy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hermosotech.myapplication
+package com.hermosotech.filmjoy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
